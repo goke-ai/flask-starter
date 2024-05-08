643a02c8ff9dc8f74982202adccbf147a2327523
goke Oladokun <gokeladokun@gmail.com>
2024-05-08 14:50:52 +0100

    Add password reset

--- app/models.py
+++ app/models.py
@@ -58,34 +58,56 @@ class User(UserMixin, db.Model):
         return True
     # -user confirmation
 
-    def generate_email_change_token(self, new_email, expiration=3600):
+    # +password reset
+    def generate_reset_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY']) #, expiration)
-        return s.dumps(
-            {'change_email': self.id, 'new_email': new_email})#.decode('utf-8')
+        return s.dumps({'reset': self.id})#.decode('utf-8')
 
-    def change_email(self, token):
+    @staticmethod
+    def reset_password(token, new_password):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)#.encode('utf-8'))
         except:
             return False
-        
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+    # -password reset
+
+    # +email change
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])  # , expiration)
+        return s.dumps(
+            {'change_email': self.id, 'new_email': new_email})  # .decode('utf-8')
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)  # .encode('utf-8'))
+        except:
+            return False
+
         if data.get('change_email') != self.id:
             return False
-        
+
         new_email = data.get('new_email')
-        
+
         if new_email is None:
             return False
-        
+
         if self.query.filter_by(email=new_email).first() is not None:
             return False
-        
+
         self.email = new_email
         # self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
-        
+
         return True
+    # -email change
 
     def __repr__(self):
         return '<User %r>' % self.username
