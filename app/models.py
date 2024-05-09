493c6a256eb06787f57a7a5a6082ba2fbd854390
goke Oladokun <gokeladokun@gmail.com>
2024-05-09 22:02:42 +0100

    Publish v0.3.2: Added profile, gravatar

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+import hashlib
 from . import login_manager
-from flask import current_app
+from flask import current_app, request
 from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 from flask_login import AnonymousUserMixin, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -22,7 +24,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
-    users = db.relationship('Users', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(Role, self).__init__(**kwargs)
@@ -57,8 +59,8 @@ class Role(db.Model):
                                Permission.EDIT, Permission.MODERATE,
                                Permission.ADMIN],
             'SystemAdministrators': [Permission.READ, Permission.ADD,
-                                    Permission.EDIT, Permission.MODERATE,
-                                    Permission.ADMIN, Permission.SYSTEM],
+                                     Permission.EDIT, Permission.MODERATE,
+                                     Permission.ADMIN, Permission.SYSTEM],
         }
 
         default_role = 'Users'
@@ -66,15 +68,15 @@ class Role(db.Model):
         for r in roles:
             role = Role.query.filter_by(name=r).first()
 
-        if role is None:
-            role = Role(name=r)
+            if role is None:
+                role = Role(name=r)
 
-        role.reset_permissions()
+            role.reset_permissions()
 
-        for perm in roles[r]:
-            role.add_permission(perm)
-            role.default = (role.name == default_role)
-            db.session.add(role)
+            for perm in roles[r]:
+                role.add_permission(perm)
+                role.default = (role.name == default_role)
+                db.session.add(role)
 
         db.session.commit()
 
@@ -85,11 +87,15 @@ class Role(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(128))
+    location = db.Column(db.String(64))
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -99,6 +105,9 @@ class User(UserMixin, db.Model):
 
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     @ property
     def password(self):
@@ -177,7 +186,7 @@ class User(UserMixin, db.Model):
             return False
 
         self.email = new_email
-        # self.avatar_hash = self.gravatar_hash()
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
 
         return True
@@ -193,6 +202,23 @@ class User(UserMixin, db.Model):
     def is_super_administrator(self):
         return self.can(Permission.SUPERADMIN)
     # -roles
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+
+        hash = self.avatar_hash or self.gravatar_hash()
+        return f'{url}/{hash}?s={size}&d={default}&r={rating}'
 
     def __repr__(self):
         return '<User %r>' % self.username
--- flasky.py
+++ flasky.py
@@ -2,6 +2,7 @@ import os
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
+from app.db_initialize import DbInitialize
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -9,7 +10,9 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role
+                # , DbInitialize=DbInitialize
+                )
 
 
 @app.cli.command()
